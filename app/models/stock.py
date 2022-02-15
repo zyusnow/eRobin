4bb64c69a9be4f7d3554ab630664e0b22540d444
Zhen <zyuluna@gmail.com>
2022-02-15 13:43:11 -0800

    stock modal

--- app/models/stock.py
+++ app/models/stock.py
@@ -3,4 +3,15 @@ from datetime import datetime
 
 
 class Stock(db.Model):
-    
+    __tablename__ = 'stocks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.VARCHAR, nullable=False)
+    company_name = db.Column(db.VARCHAR, nullable=False)
+
+    def to_dict(self):
+        return {
+          'id': self.id,
+          'ticker': self.ticker,
+          'company_name': self.company_name
+        }
