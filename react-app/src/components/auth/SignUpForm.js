2d146ad097ee7c8e53137afcdce2df00ecc8714d
Zhen <zyuluna@gmail.com>
2022-02-16 09:10:36 -0800

    signup

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -50,74 +50,86 @@ const SignUpForm = () => {
   return (
     <div className="signup_container">
       <div className='signup_left'>
-        <div className="signup_title">eRobin</div>
-        <div className='signup_guide'>
-          <div className='signup_heading'>Make Your Money Move</div>
-          <div className='signup_sub_heading'>eRobin lets you invest in companies you love, commission-free.</div>
-          <div className='signup_requirement'>Please enter your full legal name. Your legal name should match any form of government ID.</div>
-        </div>
-        <form onSubmit={onSignUp}>
-          <div>
-            {errors.map((error, ind) => (
-              <div key={ind}>{error}</div>
-            ))}
+        <div className='signup_left_sub'>
+          <div className="signup_title">eRobin</div>
+          <div className='signup_guide'>
+            <div className='signup_heading'>Make Your Money Move</div>
+            <div className='signup_sub_heading'>eRobin lets you invest in companies you love, commission-free.</div>
+            <div className='signup_requirement'>Please enter your full legal name. Your legal name should match any form of government ID.</div>
           </div>
-          <div className='full_name'>
-            <input
-              type='text'
-              name='firstName'
-              onChange={updateFirstName}
-              value={firstName}
-              placeholder="First name"
-              required
-            ></input>
-            <input
-              type='text'
-              name='lastName'
-              onChange={updateLastName}
-              value={lastName}
-              placeholder="Last name"
-              required
-            ></input>
-          </div>
-          <div>
-            <input
-              type='text'
-              name='username'
-              onChange={updateUsername}
-              value={username}
-              placeholder="Username"
-              required
-            ></input>
-          </div>
-          <div>
-            <input
-              type='text'
-              name='email'
-              onChange={updateEmail}
-              value={email}
-              placeholder="Email"
-              required
-            ></input>
-          </div>
-          <div>
-            <input
-              type='password'
-              name='password'
-              onChange={updatePassword}
-              value={password}
-              placeholder="Password"
-              required
-            ></input>
-          </div>
-          <div>
-            <button type='submit'>Sign Up</button>
+          <form id="sign_up_form" onSubmit={onSignUp}>
             <div>
-              <div>Already started?</div>
-              <NavLink to="/login">Log in to complete your application</NavLink>
+              {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+              ))}
             </div>
+            <div className='full_name'>
+              <input
+                type='text'
+                name='firstName'
+                onChange={updateFirstName}
+                value={firstName}
+                placeholder="First name"
+                required
+              ></input>
+              <input
+                type='text'
+                name='lastName'
+                onChange={updateLastName}
+                value={lastName}
+                placeholder="Last name"
+                required
+              ></input>
+            </div>
+              <input
+                type='text'
+                name='username'
+                onChange={updateUsername}
+                value={username}
+                placeholder="Username"
+                required
+              ></input>
+              <input
+                type='text'
+                name='email'
+                onChange={updateEmail}
+                value={email}
+                placeholder="Email"
+                required
+              ></input>
+              <input
+                type='password'
+                name='password'
+                onChange={updatePassword}
+                value={password}
+                placeholder="Password"
+                required
+              ></input>
+            <div>
+              <button className="signup_btn" type='submit'>Sign Up</button>
+              <div>
+                <div>Already started?</div>
+                <NavLink to="/login">Log in to complete your application</NavLink>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className='signup_right'>
+        <div className='signup_right_sub'>
+          <div className="signup_right_info">
+            <div className="signup_right_title">Commission-free trading</div>
+            <div className="signup_right_content">Break free from commission-fees and make unlimited commission-free trades in stocks, funds, and options with Robinhood Financial. Other fees may apply. View our fee schedule to learn more.</div>
           </div>
-        </form>
+          <div className="signup_right_info">
+            <div className="signup_right_title">Account Protection</div>
+            <div className="signup_right_content">eRobin is a member of SIPC. Securities in your account protected up to $500,000. For details, please see www.sipc.org.</div>
+          </div>
+          <div className="signup_right_info">
+            <div className="signup_right_title">Stay on top of your portfolio</div>
+            <div className="signup_right_content">Set up customized news and notifications to stay on top of your assets as casually or as relentlessly as you like. Controlling the flow of info is up to you.</div>
+          </div>
+        </div>
       </div>
     </div>
   );
