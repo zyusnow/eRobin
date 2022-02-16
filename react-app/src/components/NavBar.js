20801df2826eb6ea584309d26a80be616f6231d2
Zhen <zyuluna@gmail.com>
2022-02-15 22:53:44 -0800

    navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,59 @@
 
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.session.user)
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+    <nav id="navbar">
+      {
+        !user && (
+          <div className='navbar_container'>
+            <div className='navbar_left'>
+              <NavLink id="nav_head" to='/' exact={true}>
+                eRobin
+              </NavLink>
+            </div>
+            <div className='navbar_right'>
+              
+            </div>
+          </div>
+        )
+
+
+      }
     </nav>
+
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       <NavLink to='/' exact={true} activeClassName='active'>
+    //         Home
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to='/login' exact={true} activeClassName='active'>
+    //         Login
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to='/sign-up' exact={true} activeClassName='active'>
+    //         Sign Up
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <NavLink to='/users' exact={true} activeClassName='active'>
+    //         Users
+    //       </NavLink>
+    //     </li>
+    //     <li>
+    //       <LogoutButton />
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 }
 
