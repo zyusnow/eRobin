b9f6bc1ef1a3027b2ffdf2f410fb551a7e7be584
Zhen <zyuluna@gmail.com>
2022-02-17 00:00:11 -0800

    routes

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.search_routers import search_routes
+from .api.stockdetail_routes import stockdetail_routes
 
 from .seeds import seed_commands
 
@@ -33,6 +34,8 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(search_routes, url_prefix='/api/search')
+app.register_blueprint(stockdetail_routes, url_prefix='/api/stocks')
+
 db.init_app(app)
 Migrate(app, db)
 
---
+++ app/api/stockdetail_routes.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, jsonify
+import requests
+import os
+
+api_token = ""
+
+# collect the basic info of a given stock
+def fetch_stock_info(symbol):
+    stock_info = {}
+
+    request_url = "https://yh-finance.p.rapidapi.com/stock/v2/get-profile"
+    request_string = {"symbol": symbol.upper(), "region":"US"}
+    request_headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': api_token#os.environ.get("RAPID_API_KEY")
+    }
+
+    r = requests.request("GET", request_url, headers=request_headers, params=request_string)
+    if r.status_code == requests.codes.ok:
+        raw_stock_json = r.json()
+        stock_info['price'] = raw_stock_json["price"]["regularMarketPrice"]["raw"]
+        stock_info['name'] = raw_stock_json["price"]["longName"]
+        stock_info['about'] = raw_stock_json["assetProfile"]["longBusinessSummary"]
+        stock_info['employees'] =raw_stock_json["assetProfile"]["longBusinessSummary"]
+        stock_info["state"] = raw_stock_json["assetProfile"]["state"]
+        stock_info["sector"] = raw_stock_json["assetProfile"]["sector"]
+        stock_info["marketcap"] = raw_stock_json["price"]["marketCap"]["fmt"]
+        stock_info["peratio"] = raw_stock_json["summaryDetail"]["forwardPE"]["fmt"]
+
+    return stock_info
+
+def fetch_stock_price(symbol):
+    request_url = "https://alpha-vantage.p.rapidapi.com/query"
+
+    request_string = {"function":"TIME_SERIES_DAILY", "symbol": symbol.upper(), "outputsize":"compact", "datatype":"json"}
+
+    headers = {
+    'x-rapidapi-host': "alpha-vantage.p.rapidapi.com",
+    'x-rapidapi-key': api_token
+    }
+
+    dates = []
+    prices = []
+
+    r = requests.request("GET", request_url, headers=headers, params=request_string)
+    if r.status_code == requests.codes.ok:
+        price_info = r.json()
+
+        price_info = price_info["Time Series (Daily)"]
+
+        for date, info in price_info.items():
+            dates.append(date)
+            prices.append(info['4. close'])
+
+    return dates, prices
+
+stockdetail_routes = Blueprint("stocks", __name__)
+@stockdetail_routes.route("/<ticker>")
+def get_stock_info(ticker):
+
+    stock_info = fetch_stock_info(ticker)
+    dates, prices = fetch_stock_price(ticker)
+
+    stock_info['date'] = dates
+    stock_info['prices'] = prices
+
+    return jsonify(stock_info)
